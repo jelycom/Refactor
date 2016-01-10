47d12653c49bb65aee26bb66b92c36cd4273de69
Administrator <jelycom@hotmail.com>
2016-01-10 17:18:53 +0800

    将Customer的消费金额和消费积分分离:  先分离消费金额

--- src/main/java/cn/jely/cd/Customer.java
+++ src/main/java/cn/jely/cd/Customer.java
@@ -26,18 +26,28 @@ public class Customer {
         this.name = name;
     }
     public String statement() {
-        double totalAmount = 0;
         int frequentRenterPoints =0;
         String result = "Rental Record for" + getName() + "\n";
         Enumeration rentals = this.rentals.elements();
+        double totalAmount = getTotalCharge();
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
             frequentRenterPoints += each.getFrequentRenterPoints();
             result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
             totalAmount += each.getCharge();
-            }
+        }
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent reter points";
+        return result;
+    }
+
+    private double getTotalCharge() {
+        double result = 0;
+        Enumeration elements = rentals.elements();
+        while (elements.hasMoreElements()) {
+            Rental each = (Rental) elements.nextElement();
+            result += each.getCharge();
+        }
         return result;
     }
 
